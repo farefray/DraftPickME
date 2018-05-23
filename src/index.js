274b4915f28b267a04a0349c3b750ca1ba3e0421
Unknown <maksym.fedan@gmail.com>
2018-05-23 23:40:50 +0300

    hello world :)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-// Set up your application entry point here...
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
