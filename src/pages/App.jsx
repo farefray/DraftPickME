32e86f5ae27b60370b8d63b15a3e2205be625c10
Unknown <maksym.fedan@gmail.com>
2018-08-01 11:52:23 +0300

    some minor changes and cleanups

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,34 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Router, Route, Switch } from "react-router-dom";
 import { PrivateRoute } from "./components/PrivateRoute";
-
 import { CSSTransition, TransitionGroup } from "react-transition-group";
-
 import { NProgress } from "redux-nprogress";
+import { Alerts } from "./components/Alerts";
+import history from "../helpers/history";
 
 import Homepage from "./app/Homepage";
 import { Login } from "./app/Login";
 import { Register } from "./app/Register";
 import { Profile } from "./app/Profile";
 import { EditProfile } from "./app/login/EditProfile";
-import { Alerts } from "./components/Alerts";
-import history from "../helpers/history";
+
 import { firebase } from "@/firebase";
-
-const PageFade = props => (
-  <CSSTransition
-    {...props}
-    classNames="fadeTranslate"
-    timeout={1000}
-    mountOnEnter={true}
-    unmountOnExit={true}
-  />
-);
-
-const Layout = ({ children }) => <div id="root">{children}</div>;
-Layout.propTypes = {
-  children: PropTypes.array.isRequired
-}
 
 const RouteWithAuth = ({ component: Component, path, authUser, ...rest }) => (
   <Route
@@ -46,14 +30,16 @@ RouteWithAuth.propTypes = {
 };
 
 const Main = props => {
-  const locationKey = props.location.pathname;
-
-  console.log('main props');
-  console.log(props);
   return (
-    <Layout>
+    <div id="root">
       <TransitionGroup>
-        <PageFade key={locationKey}>
+        <CSSTransition
+            key={props.location.pathname}
+            classNames="fadeTranslate"
+            timeout={1000}
+            mountOnEnter={true}
+            unmountOnExit={true}
+          >
           <Switch location={props.location}>
             <RouteWithAuth
               exact
@@ -69,14 +55,14 @@ const Main = props => {
             />
             <Route path="/register" component={Register} />
           </Switch>
-        </PageFade>
+        </CSSTransition>
       </TransitionGroup>
       <RouteWithAuth
         path="/p/:username"
         component={Profile}
         authUser={props.authUser}
       />
-    </Layout>
+    </div>
   );
 };
 
@@ -97,8 +83,8 @@ class App extends React.Component {
   }
 
   componentDidMount() {
+    // changing authUser by trigger from firebase (probably must be moved into Redux instead of passing by props)
     firebase.auth.onAuthStateChanged(authUser => {
-      console.log('auth changed');
       authUser
         ? this.setState({ authUser })
         : this.setState({ authUser: null });
@@ -106,8 +92,6 @@ class App extends React.Component {
   }
 
   render() {
-    console.log("App");
-    console.log(this.props);
     return (
       <div>
         <NProgress color="#78cc78" />
